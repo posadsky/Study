a408dde2a1c9fbc443f046b365d03d0dfebdf03f
posadsky <posadsky@unact.ru>
2012-11-19 13:53:11 +0400

    Залил калькулятор

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Алексей Посадский on 11/5/12.
+//  Copyright (c) 2012 unact. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Алексей Посадский on 11/5/12.
+//  Copyright (c) 2012 unact. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
